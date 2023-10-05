cc8b0a54c9787897630f1e8167af894c90af60d2
vanilla0129 <30804855@qq.com>
2023-10-06 07:54:59 +0800

    Site updated: 2023-10-06 07:54:58

--- music.js
+++
@@ -1,9 +0,0 @@
-const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    audio: [{
-        name: '',
-        artist: '',
-        url: '',
-        cover: '',
-    }]
-});
