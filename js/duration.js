5c226ce3630c0b6e57e864bb5ab0e6a3c997db8f
vanilla0129 <30804855@qq.com>
2023-10-15 17:33:56 +0800

    Site updated: 2023-10-15 17:33:55

--- js/duration.js
+++ js/duration.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>!(function() {
+!(function() {
   /** 计时起始时间，自行修改 **/
   var start = new Date("2023/10/03 00:00:00");
 
